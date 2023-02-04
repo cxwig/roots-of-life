e6b5260cfdac4ca2250b7e3d200f4a06251e021d
Vasily <vasilii.orlov1905@gmail.com>
2023-02-03 23:36:17 -0800

    hexagonmanager

---
+++ Assets/Assets/Scripts/HexagonManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexagonManager : MonoBehaviour
+{
+    public GameObject[] hexagonPrefabs;
+    public float spawnTime = 1.0f;
+    public float spawnX = 4.0f;
+    public float hexagonSideLength = 4.0f;
+
+    private float timeSincePreviousSpawn;
+    private float spawnY = 0.0f;
+    private int hexagonIndex;
+
+    private void Update()
+    {
+        timeSincePreviousSpawn += Time.deltaTime;
+        if (timeSincePreviousSpawn >= spawnTime)
+        {
+            timeSincePreviousSpawn = 0.0f;
+            SpawnHexagon();
+        }
+    }
+
+    private void SpawnHexagon()
+    {
+        hexagonIndex = Random.Range(0, hexagonPrefabs.Length);
+        Instantiate(hexagonPrefabs[hexagonIndex], new Vector2(spawnX, spawnY), Quaternion.identity);
+        spawnX += hexagonSideLength;
+    }
+}
