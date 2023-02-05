c625db80c3b161f73474152d3ccca107f0427dde
deny s <denysenesouma@gmail.com>
2023-02-05 14:42:59 -0800

    fix dependency thing

--- Assets/Assets/Scripts/FX/DespawnParticle.cs
+++ Assets/Assets/Scripts/FX/DespawnParticle.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
-using UnityEditor.VersionControl;
 using UnityEngine;
 using Task = System.Threading.Tasks.Task;
 
@@ -21,7 +20,7 @@ public class DespawnParticle : MonoBehaviour
     {
         try
         {
-            await Task.Delay((int) (_particleSystem.main.duration * 1000));
+            await Task.Delay((int) (_particleSystem.main.startLifetime.constant * 1000));
             _cancellation.Token.ThrowIfCancellationRequested();
         }
         catch(Exception e)
